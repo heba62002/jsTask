d1f9ff6f1c0d15b629304c6cba18cb9b4070a4a4
heba62002 <kasasbehheba42@gmail.com>
2024-12-12 11:25:46 +0300

    This is solve the task

---
+++ java-Task.js
@@ -0,0 +1,18 @@
+let username = prompt("please enter your username"); 
+//let pass = prompt("please enter your password"); 
+
+
+for(let i =0; i< 6 ; i++){
+   
+        password = prompt("enter the correct password ");
+        if (password == "123"){
+            alert("welcom ")
+            break ;
+        }
+        
+        else { 
+            alert("plz try again ")
+        }
+        
+  
+}
